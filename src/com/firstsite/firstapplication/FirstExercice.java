1a7919d667e92e919b12fe41135672ea007275d6
nathnaelhaile <nathnaelhaile@gmail.com>
2017-07-28 14:40:58 -0400

    These are my first changes

---
+++ src/com/firstsite/firstapplication/FirstExercice.java
@@ -0,0 +1,17 @@
+package com.firstsite.firstapplication;
+
+public class FirstExercice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int x=1; x<=100; x++)
+		{
+			if(x%2==1)
+				System.out.print(x);
+			else
+				System.out.println(x+" "+"is an even number");
+		}
+
+	}
+
+}
---
+++ src/myfirstpackage/MyFirstRunningClass.java
@@ -0,0 +1,10 @@
+package myfirstpackage;
+
+public class MyFirstRunningClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
